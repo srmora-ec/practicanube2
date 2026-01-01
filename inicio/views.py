543848aa387faa7340bd19195df2fd3fc1695972
srmora-ec <alvarezjoseandres@outlook.es>
2025-12-31 23:15:13 -0500

    Prueba github

--- inicio/views.py
+++ inicio/views.py
@@ -1,4 +1,4 @@
 from django.http import HttpResponse
 
 def hola_mundo(request):
-    return HttpResponse("<h1>¡Hola mundo desde Django!</h1>")
+    return HttpResponse("<h1>¡Hola mundo desde Django, prueba de actualización github!</h1>")
